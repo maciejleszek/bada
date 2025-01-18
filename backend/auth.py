6b60f1f97184d0857c2937e5be69b6f64a7d9106
rslezakIIR <01178333@pw.edu.pl>
2025-01-18 14:09:17 +0100

    Endpoints requiring JWT now have access to the token's data

--- backend/auth.py
+++ backend/auth.py
@@ -8,11 +8,16 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
+
         if not token:
             return jsonify({'message': 'Token is missing'}), 403
         try:
-            jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            jwt_decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         except:
             return jsonify({'message': 'Token is invalid'}), 403
+        
+        # Make decoded token available to wrapped function
+        kwargs.update({"jwt_token_decoded": jwt_decoded})
+
         return f(*args, **kwargs)
     return decorated

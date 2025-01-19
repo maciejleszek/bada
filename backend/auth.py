0f147dd64f005dc37b8c013b83fb489ffd2e49ac
rslezakIIR <01178333@pw.edu.pl>
2025-01-19 16:09:49 +0100

    Improved token detection; added expired token exception

--- backend/auth.py
+++ backend/auth.py
@@ -9,12 +9,19 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
 
+        try:
+            token = token.split(" ")[1]
+        except:
+            pass
+
         if not token:
             return "Error: Nagłówek \"Authorization\" jest wymagany", 401
         try:
             jwt_decoded = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        except:
+        except jwt.InvalidTokenError:
             return "Error: niewłaściwy token", 401
+        except jwt.ExpiredSignatureError:
+            return "Error: token wygasł", 401
         
         # Udostępnij dane w tokenie funkcji wewnętrznej
         kwargs.update({"jwt_token_decoded": jwt_decoded})
